c2bb51280ff29132b0b71759886c5aac15ece994
Thorsten Hans <thorsten.hans@gmail.com>
2016-01-07 10:50:25 +0100

    compile vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	inSequence = require('run-sequence'),
 	del = require('del'),
-	inject = require('gulp-inject');
+	inject = require('gulp-inject'),
+	concat = require('gulp-concat');
 
 
 gulp.task('private:clear', function(done){
@@ -9,7 +10,20 @@ gulp.task('private:clear', function(done){
 	done();
 });
 
+gulp.task('private:build-vendor', function(){
+	return gulp.src([
+		"node_modules/angular2/bundles/angular2-polyfills.js",
+        "node_modules/systemjs/dist/system.src.js",
+        "node_modules/rxjs/bundles/Rx.js",
+        "node_modules/angular2/bundles/angular2.dev.js",
+        "node_modules/angular2/bundles/http.dev.js",
+        "node_modules/angular2/bundles/router.dev.js"
+		])
+		.pipe(concat('vendor.js'))
+		.pipe(gulp.dest('dist/scripts'));
+});
+
 
 gulp.task('default', function(done){
-	inSequence('private:clear', done);
+	inSequence('private:clear', ['private:build-vendor'], done);
 });

ffed5c7654417ae1b8dcbcb624d7374966e130c6
Thorsten Hans <thorsten.hans@gmail.com>
2016-07-22 11:41:34 +0200

    bootstrap heroComponent instead of appComponent

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -1,4 +1,4 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
+import {HeroComponent} from './components/hero/heroComponent';
 
-bootstrap(AppComponent);
+bootstrap(HeroComponent);
